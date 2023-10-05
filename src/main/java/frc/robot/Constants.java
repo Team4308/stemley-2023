b3933b737be2b6d895d1e86dec445602ca5dd533
Cristophe Chen <Cristophe.Chen@gmail.com>
2023-10-05 15:56:17 -0400

    constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,6 +1,30 @@
 package frc.robot;
 
-public class Constants {
-    
+import com.ctre.phoenix.motorcontrol.StatorCurrentLimitConfiguration;
+
+public final class Constants {
+    public static class Mapping {
+        public static class Drive {
+        }
+    }
+
+    public static class Generic {
+        public static int timeoutMs = 3000;
+    }
+
+    public static class Config {
+        public static class Input {
+            public static double kInputDeadband = 0.14;
+
+            public static class Stick {
+                public static double kInputScale = 2.0;
+            }
+        }
+    }
+
+    public static class DynConfig {
+        public static class Drive {
+            public static double VelocityDriveRPM = 4000;
+        }
+    }
 }
-                        
