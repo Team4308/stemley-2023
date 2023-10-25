043e7af072ecf28cbef3594ea1f939c214363d38
Cristophe Chen <Cristophe.Chen@gmail.com>
2023-10-25 16:33:14 -0400

    testing some stuff

--- src/main/java/frc/robot/Subsystems/DriveSystem.java
+++ src/main/java/frc/robot/Subsystems/DriveSystem.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.NeutralMode;
-import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
-import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
-import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
+import ca.team4308.absolutelib.wrapper.MotoredSubsystem;
 import ca.team4308.absolutelib.wrapper.drive.TankDriveSubsystem;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -21,58 +21,32 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Constants;
 
-public class DriveSystem extends TankDriveSubsystem {
+public class DriveSystem extends MotoredSubsystem{
         // Master Controllers
-        public final TalonFX masterLeft, masterRight;
+        public final TalonSRX masterLeft;
         // Slave Controllers
-        private final TalonFX slaveLeft, slaveRight;
-
-        //IMU
-        public static ADIS16470_IMU gyro = new ADIS16470_IMU();
-        // Beambreaks
-        public static DigitalInput lineBreak;
-        
-
-
-        public static Boolean brakeMode = false;
 
 
         // Controllers
-        private ArrayList<TalonFX> controllersFX = new ArrayList<TalonFX>();
+        private ArrayList<TalonSRX> controllersSRX = new ArrayList<TalonSRX>();
 
         // Init
         public DriveSystem() {
                 // Setup and Add Controllers
-                masterLeft = new TalonFX(Constants.Mapping.Drive.frontLeft);
-                controllersFX.add(masterLeft);
-                masterRight = new TalonFX(Constants.Mapping.Drive.frontRight);
-                controllersFX.add(masterRight);
-                slaveLeft = new TalonFX(Constants.Mapping.Drive.backLeft);
-                controllersFX.add(slaveLeft);
-                slaveRight = new TalonFX(Constants.Mapping.Drive.backRight);
-                controllersFX.add(slaveRight);
+                masterLeft = new TalonSRX(Constants.Mapping.Drive.frontLeft);
+                controllersSRX.add(masterLeft);
                 
                 // Reset Config for all
-                for (TalonFX talon : controllersFX) {
+                for (TalonSRX talon : controllersSRX) {
                         talon.configFactoryDefault(Constants.Generic.timeoutMs);
                 }
 
-                // Set Invert Mode
-                masterLeft.setInverted(TalonFXInvertType.CounterClockwise);
-                masterRight.setInverted(TalonFXInvertType.Clockwise);
-                slaveLeft.follow(masterLeft);
-                slaveLeft.setInverted(TalonFXInvertType.FollowMaster);
-                slaveRight.follow(masterRight);
-                slaveRight.setInverted(TalonFXInvertType.FollowMaster);
-
                 // Change Config For All Controllers
-                for (TalonFX talon : controllersFX) {
+                for (TalonSRX talon : controllersSRX) {
                         talon.configFactoryDefault(Constants.Generic.timeoutMs);
                         talon.configOpenloopRamp(Constants.Config.Drive.Power.kOpenLoopRamp,
                                         Constants.Generic.timeoutMs);
                         talon.configClosedloopRamp(Constants.Config.Drive.Power.kClosedLoopRamp,
-                                        Constants.Generic.timeoutMs);
-                        talon.configStatorCurrentLimit(Constants.Config.Drive.Power.kStatorCurrentLimit,
                                         Constants.Generic.timeoutMs);
                         talon.setNeutralMode(NeutralMode.Coast);
                         talon.configNeutralDeadband(0.001, Constants.Generic.timeoutMs);
@@ -81,67 +55,13 @@ public class DriveSystem extends TankDriveSubsystem {
                         talon.enableVoltageCompensation(true);
                 }
 
-                // Configure Primary Closed Loop Sensor
-                masterLeft.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0,
-                                Constants.Generic.timeoutMs);
-                masterRight.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0,
-                                Constants.Generic.timeoutMs);
-
                 // Set Sensor Phase for all loops
                 masterLeft.setSensorPhase(false);
                 // masterRight.setSensorPhase(false);
 
-                // Set Left Velocity PIDF values
-                masterLeft.config_kP(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Left.kP, Constants.Generic.timeoutMs);
-                masterLeft.config_kI(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Left.kI, Constants.Generic.timeoutMs);
-                masterLeft.config_kD(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Left.kD, Constants.Generic.timeoutMs);
-                masterLeft.config_kF(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Left.kF, Constants.Generic.timeoutMs);
-                masterLeft.selectProfileSlot(Constants.Config.Drive.VelocityControl.profileSlot, 0);
-
-                // Set Right Velocity PIDF values
-                masterRight.config_kP(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Right.kP, Constants.Generic.timeoutMs);
-                masterRight.config_kI(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Right.kI, Constants.Generic.timeoutMs);
-                masterRight.config_kD(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Right.kD, Constants.Generic.timeoutMs);
-                masterRight.config_kF(Constants.Config.Drive.VelocityControl.profileSlot,
-                                Constants.Config.Drive.VelocityControl.Right.kF, Constants.Generic.timeoutMs);
-                masterRight.selectProfileSlot(Constants.Config.Drive.VelocityControl.profileSlot, 0);
-
-                masterLeft.configMotionCruiseVelocity(Constants.Config.Drive.MotionMagic.maxVel);
-                masterRight.configMotionCruiseVelocity(Constants.Config.Drive.MotionMagic.maxVel);
-                masterLeft.configMotionAcceleration(Constants.Config.Drive.MotionMagic.maxAcc);
-                masterRight.configMotionAcceleration(Constants.Config.Drive.MotionMagic.maxAcc);
-
-                masterLeft.config_kP(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kP, Constants.Generic.timeoutMs);
-                masterLeft.config_kI(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kI, Constants.Generic.timeoutMs);
-                masterLeft.config_kD(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kD, Constants.Generic.timeoutMs);
-                masterLeft.config_kF(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kF, Constants.Generic.timeoutMs);
-                masterLeft.setNeutralMode(NeutralMode.Coast);
-                masterLeft.config_IntegralZone(Constants.Config.Drive.MotionMagic.profileSlot, 10);
-                masterRight.config_kP(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kP, Constants.Generic.timeoutMs);
-                masterRight.config_kI(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kI, Constants.Generic.timeoutMs);
-                masterRight.config_kD(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kD, Constants.Generic.timeoutMs);
-                masterRight.config_kF(Constants.Config.Drive.MotionMagic.profileSlot,
-                                Constants.Config.Drive.MotionMagic.Right.kF, Constants.Generic.timeoutMs);
-                masterRight.setNeutralMode(NeutralMode.Coast);
-                masterRight.config_IntegralZone(Constants.Config.Drive.MotionMagic.profileSlot, 10);
 
                 // Reset
                 stopControllers();
-                resetSensors();
         }
 
         /**
@@ -153,8 +73,7 @@ public class DriveSystem extends TankDriveSubsystem {
         }
 
         public double getRightSensorPosition() {
-                return masterRight.getSelectedSensorPosition(0)
-                                * Constants.Config.Drive.Kinematics.kEncoderInchesPerCount;
+                return 0;
         }
 
         public double getLeftSensorVelocity() {
@@ -162,33 +81,21 @@ public class DriveSystem extends TankDriveSubsystem {
         }
 
         public double getRightSensorVelocity() {
-                return masterRight.getSelectedSensorVelocity(0);
+                return 0;
         }
 
         
-        public void setMotorOutput(ControlMode mode, double left, double right) {
-                masterLeft.set(mode, left);
-                masterRight.set(mode, right);
+        public void setMotorOutput() {
+                masterLeft.set(TalonSRXControlMode.PercentOutput, 0.5);
         }
 
         public void selectProfileSlot(int slot) {
                 masterLeft.selectProfileSlot(slot, 0);
-                masterRight.selectProfileSlot(slot, 0);
         }
 
-        public void resetAngle() {
-                gyro.reset();
-        }
 
         public void stopControllers() {
-                masterLeft.set(TalonFXControlMode.PercentOutput, 0.0);
-                masterRight.set(TalonFXControlMode.PercentOutput, 0.0);
-        }
-
-        // Sensor Reset
-        public void resetSensors() {
-                masterLeft.setSelectedSensorPosition(0);
-                masterRight.setSelectedSensorPosition(0);
+                masterLeft.set(TalonSRXControlMode.PercentOutput, 0.0);
         }
 
         @Override
